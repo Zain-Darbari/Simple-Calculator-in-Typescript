1d51cb581f9d68d07d69c529e7c78bae6c35b5b4
Zain_Darbari <topmafiyagamer@gmail.com>
2024-04-18 19:51:47 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{
+    message:"Enter first Number", 
+    type:"number",
+    name:"firstNumber"},
+
+    {
+        message:"Enter second Number", 
+        type:"number",
+        name:"secondNumber"
+    },
+    {message:"Select one of the operators to perform of action", 
+type: "list",
+name:"Operator",
+choices:["Addition", "Subtraction", "Multiplication","Division", "Modulo"],
+}
+]);
+
+if (answer.Operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+
+else if (answer.Operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+
+else if (answer.Operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+
+else if (answer.Operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber)
+} else{
+
+console.log("Please select valid operator")
+}
